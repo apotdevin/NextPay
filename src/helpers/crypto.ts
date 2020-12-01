d334669f3c1a3b1a3a878f45440e7460fb0fc3b3
apotdevin <potdevin.anthony@gmail.com>
2020-12-01 22:08:32 +0100

    chore: 🔧 encode bytearray

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,9 +1,44 @@
-import { createHash, randomBytes } from 'crypto';
+import { BinaryLike, createHash, randomBytes } from 'crypto';
 
-export const getHash = (str: string, encoding: 'hex' | 'base64' = 'hex') =>
-  createHash('sha256').update(str).digest(encoding);
+export const getHash = (
+  str: string | BinaryLike,
+  encoding: 'hex' | 'base64' = 'hex'
+) => createHash('sha256').update(str).digest(encoding);
 
 export const getRandomBytes = (
   length = 64,
   encoding: 'hex' | 'base64' = 'hex'
 ) => randomBytes(length).toString(encoding);
+
+export const toUTF8Array = (str: string): BinaryLike => {
+  const utf8 = [];
+  for (let i = 0; i < str.length; i++) {
+    let charcode = str.charCodeAt(i);
+    if (charcode < 0x80) utf8.push(charcode);
+    else if (charcode < 0x800) {
+      utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f));
+    } else if (charcode < 0xd800 || charcode >= 0xe000) {
+      utf8.push(
+        0xe0 | (charcode >> 12),
+        0x80 | ((charcode >> 6) & 0x3f),
+        0x80 | (charcode & 0x3f)
+      );
+    }
+    // surrogate pair
+    else {
+      i++;
+      // UTF-16 encodes 0x10000-0x10FFFF by
+      // subtracting 0x10000 and splitting the
+      // 20 bits of 0x0-0xFFFFF into two halves
+      charcode =
+        0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
+      utf8.push(
+        0xf0 | (charcode >> 18),
+        0x80 | ((charcode >> 12) & 0x3f),
+        0x80 | ((charcode >> 6) & 0x3f),
+        0x80 | (charcode & 0x3f)
+      );
+    }
+  }
+  return utf8;
+};
