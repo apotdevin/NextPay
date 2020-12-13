a6e586412a772970e857f846e12d0b32a26d9347
apotdevin <potdevin.anthony@gmail.com>
2020-12-13 16:23:07 +0100

    fix: 🐛 min max amount

--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { getHash } from 'src/helpers/crypto';
 import { middlewares } from 'src/helpers/express';
 import { env, metadata } from 'src/helpers/env';
+import { absoluteUrl } from 'src/helpers/url';
 
 export default async function handler(req: Request, res: Response) {
   await middlewares(req, res);
@@ -10,10 +11,12 @@ export default async function handler(req: Request, res: Response) {
   const ip = getIp(req);
   const ipHash = getHash(ip);
 
+  const { origin } = absoluteUrl(req);
+
   const response = {
-    callback: `${env.url}/api/v1/pay/${ipHash}`,
-    maxSendable: env.max,
-    minSendable: env.min,
+    callback: `${origin}/api/v1/pay/${ipHash}`,
+    maxSendable: env.max * 1000,
+    minSendable: env.min * 1000,
     metadata,
     commentAllowed: 0,
     tag: 'payRequest',
